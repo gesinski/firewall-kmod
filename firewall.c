824c80d008da3112a7dacebc2b5cb29dbe2f5e98
gesinski <gesinskipawel@gmail.com>
2026-01-04 16:25:20 +0100

    protocols headers size check & logs info

--- firewall.c
+++ firewall.c
@@ -9,30 +9,62 @@
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("gesinski");
-MODULE_DESCRIPTION("Simple firewall module");
-MODULE_VERSION("1.1");
+MODULE_DESCRIPTION("Simple firewall");
+MODULE_VERSION("1.2");
 
 static struct nf_hook_ops *nfho = NULL;
 
 static unsigned int hfunc(void *priv, struct sk_buff *skb, const struct nf_hook_state *state)
 {
 	struct iphdr *iph;
+    struct tcphdr *tcph;
 	struct udphdr *udph;
+
 	if (!skb)
 		return NF_ACCEPT;
 
 	iph = ip_hdr(skb);
-	if (iph->protocol == IPPROTO_UDP) {
-		udph = udp_hdr(skb);
-		if (ntohs(udph->dest) == 53) {
-			return NF_ACCEPT;
-		}
-	}
-	else if (iph->protocol == IPPROTO_TCP) {
+	if(!iph)
 		return NF_DROP;
-	}
-	
-	return NF_ACCEPT;
+
+	if (!pskb_may_pull(skb, iph->ihl*4))
+        return NF_DROP;
+
+	switch (iph->protocol) {
+        case IPPROTO_ICMP:
+            return NF_ACCEPT;
+
+        case IPPROTO_UDP:
+			if (!pskb_may_pull(skb, iph->ihl*4 + sizeof(struct udphdr)))
+                return NF_DROP;
+
+            udph = udp_hdr(skb);
+
+            if (ntohs(udph->dest) == 53 || ntohs(udph->source) == 53) 
+                return NF_ACCEPT;
+			else 
+				printk(KERN_INFO "Dropped packet: src=%pI4 dst=%pI4 proto=%d port=%d\n",
+					   &iph->saddr, &iph->daddr, iph->protocol, ntohs(udph->dest));
+            break;
+
+        case IPPROTO_TCP:
+			if (!pskb_may_pull(skb, iph->ihl*4 + sizeof(struct tcphdr)))
+				return NF_DROP;
+
+            tcph = tcp_hdr(skb);
+
+            if (ntohs(tcph->dest) == 22 || ntohs(tcph->dest) == 80 || ntohs(tcph->dest) == 443)
+                return NF_ACCEPT;
+			else 
+				printk(KERN_INFO "Dropped packet: src=%pI4 dst=%pI4 proto=%d port=%d\n",
+               		   &iph->saddr, &iph->daddr, iph->protocol, ntohs(tcph->dest));
+            break;
+    }
+
+	printk(KERN_INFO "Dropped packet: src=%pI4 dst=%pI4 proto=%d\n",
+    	   &iph->saddr, &iph->daddr, iph->protocol);
+
+    return NF_DROP;
 }
 
 static int __init firewall_init(void)
